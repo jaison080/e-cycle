a3acd7fa198facbde64ff4d94eb08c3d15d6bd40
jaison080 <jaisondennis080@gmail.com>
2022-11-26 04:10:36 +0530

    Added auth

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styles from "./Landing.module.css";
+import {signIn} from "next-auth/react";
+
 function Landing() {
   return (
     <div className={styles.landing_container}>
         <div className={styles.left}>
             <h1>Reuse, Recycle, Reduce</h1>
             <p>Reduce your carbon footprint by reusing and recycling your old appliances</p>
-            <button>Get Started</button>
+            <button className={styles.button} onClick={()=>{signIn('google')}}>Get Started</button>
         </div>
     </div>
   );
