f76ebadda991183d293d77613f75b035277c88cb
Mayon-Francis <mayonfrancis@gmail.com>
2022-11-26 04:57:31 +0530

    logo

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 import styles from "./Landing.module.css";
+import logo from "../../assets/ecycle.png";
+
 function Landing() {
   return (
     <div className={styles.landing_container}>
-        <div className={styles.left}>
-            <h1>Reuse, Recycle, Reduce</h1>
-            <p>Reduce your carbon footprint by reusing and recycling your old appliances</p>
-            <button>Get Started</button>
-        </div>
+      <Image
+        src={logo}
+        style={{
+          position: "absolute",
+          top: "50px",
+          left: "50%",
+          transform: "translate(-50%, 0%)",
+        }}
+      />
+      <div className={styles.left}>
+        <h1>Reuse, Recycle, Reduce</h1>
+        <p>
+          Reduce your carbon footprint by reusing and recycling your old
+          appliances
+        </p>
+        <button>Get Started</button>
+      </div>
     </div>
   );
 }
 
-export default Landing
+export default Landing;
