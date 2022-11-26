e31ea58ba600d0d576824fed20408e9ebd5ba7a5
Mayon-Francis <mayonfrancis@gmail.com>
2022-11-26 06:29:41 +0530

    userDashboard

---
+++ src/pages/userDashboard.js
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import AddProduct from "../components/userDashboard/AddProduct";
+import Carousel from "../components/userDashboard/Carousel";
+import Navbar from "../components/userDashboard/Navbar";
+import styles from "../styles/userDashboard.module.css";
+export default function userDashboard() {
+  return (
+    <div className={styles.wrapper}>
+      <Navbar />
+      <AddProduct />
+      <Typography variant="h4" >Your Products</Typography>
+      <Carousel />
+    </div>
+  );
+}
