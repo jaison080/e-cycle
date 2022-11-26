c4342751fc53eac33ef140fcce69a7280ef1f31d
jaison080 <jaisondennis080@gmail.com>
2022-11-26 10:12:33 +0530

    AOS for Landing Complete

--- src/pages/userDashboard.js
+++ src/pages/userDashboard.js
@@ -4,9 +4,11 @@ import AddProduct from "../components/userDashboard/AddProduct";
 import Carousel from "../components/userDashboard/Carousel";
 import Navbar from "../components/userDashboard/Navbar";
 import styles from "../styles/userDashboard.module.css";
+import CustomTitle from "../utils/customTitle";
 export default function userDashboard() {
   return (
     <>
+    <CustomTitle title="User Dashboard"/>
       <Navbar />
       <div className={styles.wrapper}>
       <AddProduct />
